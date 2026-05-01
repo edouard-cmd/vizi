2921ac9aab1a14b4bae4b31473c1b73e43363199
edouard-cmd <edouard@cobound.fr>
2026-05-01 23:19:48 +0200

    Update vizi-app.js

--- vizi-app.js
+++ vizi-app.js
@@ -6803,7 +6803,7 @@ function vzmInit() {
           '<path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"/>' +
           '<circle cx="12" cy="12" r="3"/>' +
         '</svg>' +
-        '<span>Partager ma visibilité</span>' +
+        '<span>Partager la visibilité</span>' +
       '</button>' +
       '<button id="vzmSaveSessionBtn" class="vzm-action-btn vzm-action-secondary">' +
         '<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="flex-shrink:0;">' +
